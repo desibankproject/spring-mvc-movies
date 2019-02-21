1d94bd32d08b68f27626e6c5987b3f7384bf417d
Synergistisitit <synergisticit2020@gmail.com>
2019-02-21 14:39:26 -0800

    JavaScript Day-1 + Dom

--- src/main/java/com/jocker/controller/GreetingController.java
+++ src/main/java/com/jocker/controller/GreetingController.java
@@ -6,11 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //@Controller is mandatory here
 @Controller
 public class GreetingController {
 	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 	
 	public GreetingController(){
 		System.out.println("GreetingControllerGreetingControllerGreetingControllerGreetingControllerGreetingControllerGreetingController");
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,23 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Hey!</title>
+  <script type="text/javascript">
+
+		calFact();
+		
+		function calFact() {
+			var num=5;
+			var f=1;
+			for(var x=2;x<=num;x++){
+				 f=f*x; 
+			}
+			console.log("Factorial of "+num +" is  "+f);
+		}
+  </script>
+</head>
+<body>
+	<h1>Hey we are learning JavaScript</h1>
+</body>
+</html>
